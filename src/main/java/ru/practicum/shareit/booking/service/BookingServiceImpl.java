8a462d76a981a97c915b8238488f2a8feec05306
edward <hammer_head@mail.ru>
2023-02-11 14:30:06 +0400

    fix: unused import

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -20,7 +20,6 @@ import ru.practicum.shareit.mapper.ModelMapper;
 import ru.practicum.shareit.security.facade.IAuthenticationFacade;
 import ru.practicum.shareit.security.user.ExtendedUserDetails;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
